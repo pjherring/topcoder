72e3b304cd539fbf8ef5d4b4930061942caa6e27
pherring@hugeinc.com <pherring@hugeinc.com>
2013-03-05 10:18:15 -0500

    solved it!

--- graph.php
+++ graph.php
@@ -7,12 +7,34 @@ class Vertex {
     public function __construct() {
         $this->edges = array();
     }
+
+    public function display() {
+        echo "{\n\tedges : [\n";
+        foreach($this->edges as $edge) {
+            echo "\t";
+            $edge->display();
+        }
+
+        echo "\tindex : {$this->index} \n}\n";
+    }
 }
 
 class Edge {
     public $vertex_a;
     public $vertex_b;
     public $weight;
+    public $id;
+
+    public function __construct() {
+        $this->id = uniqid($prefix = "edge", $more_entropy = true);
+    }
+
+    public function display() {
+        echo "{\n\ta : {$this->vertex_a->index},\n\tb : " .
+            "{$this->vertex_b->index},\n\t" .
+            "weight : {$this->weight}\n}" . PHP_EOL;
+    }
+
 }
 
 class Path {
@@ -26,11 +48,25 @@ class Path {
         $this->length = 0;
     }
 
-    public function add($vertex, $edge) {
-        $vertices[] = $vertex;
-        $edges[] = $edge;
-        $length += $edge->weight;
+    public function add(Edge $edge) {
+
+        if ($edge) {
+            $this->edges[] = $edge;
+            /**
+             * Only add vertex_a the first time so we avoid duplicate
+                 vertex entries
+             **/
+            if (empty($this->vertices)) {
+                echo "adding vertex {$edge->vertex_a->index} to path" . PHP_EOL;
+                $this->vertices[] = $edge->vertex_a;
+            }
+
+            echo "adding vertex {$edge->vertex_b->index} to path" . PHP_EOL;
+            $this->vertices[] = $edge->vertex_b;
+            $this->length += $edge->weight;
+        }
     }
+
 }
 
 /*
@@ -59,23 +95,23 @@ function setup_vertices($cnt, $max_weight) {
     $vertices = array();
 
     for ($i = 0; $i < $cnt; $i++) {
-        $vertices[] = new Vertex();
-        $vertices[$i]->edges = array();
-        $vertices[$i]->index = $i;
-
+        $vertex = new Vertex();
+        $vertex->index = $i;
+        $vertices[] = $vertex;
     }
 
     for ($i = 0; $i < $cnt; $i++) {
 
-        for ($j = 0; $j < $cnt / 2; $j++) {
+        for ($j = 0; $j < $cnt; $j++) {
 
             $vertex_b = rand(0, $cnt - 1);
 
             if ($vertex_b != $i) {
                 $edge = new Edge();
-                $edge->vertex_a = $i;
-                $edge->vertext_b = $vertext_b;
+                $edge->vertex_a = $vertices[$i];
+                $edge->vertex_b = $vertices[$vertex_b];
                 $edge->weight = rand(1, $max_weight);
+                $vertices[$i]->edges[] = $edge;
             }
         }
     }
@@ -83,66 +119,152 @@ function setup_vertices($cnt, $max_weight) {
     return $vertices;
 }
 
+function store_path(Path $path, array &$paths) {
+
+    $index = "";
+    foreach ($path->vertices as $vertex) {
+        $index .= $vertex->index;
+    }
+
+    echo "storing path as $index" . PHP_EOL;
+    $paths[$index] = clone $path;
+}
+
+function retrieve_path(array $vertices, array $paths) {
+
+    $index = "";
+    foreach($vertices as $vertex) {
+        $index .= $vertex->index;
+    }
+
+    $path_keys = array_keys($paths);
+    foreach($path_keys as $path_key) {
+        if ($path_key == $index) {
+            echo "retrieving path for $path_key" . PHP_EOL;
+            return clone $paths[$path_key];
+        }
+    }
+
+    echo "did not find an index for '$index'" . PHP_EOL;
+
+    return false;
+}
+
+function edge_sort($edge_a, $edge_b) {
+    if ($edge_a->weight == $edge_b->weight) {
+        return 0;
+    }
+
+    return $edge_a->weight > $edge_b->weight ? -1 : 1;
+}
+
+
 $cnt = 10;
 
-$looking_for_vertex = rand(0, $cnt - 1);
-$shortest_path = -1;
+$looking_for_vertex = rand(1, $cnt - 1);
+
+$shortest_path = false;
 $max_weight = $cnt * 3;
 $vertices = setup_vertices($cnt, $max_weight);
 
 //we need to keep track of visited edges
 $visited_edges = array();
 /**
- * Stack will contain what vertices we can get to
+ * Stack will contain what edges we can get to
  **/
-$stack = array();
+$stack = $vertices[0]->edges;
+uasort($stack, 'edge_sort');
 /**
  * Current path is the current path we are constructing, it will contain 
  **/
 $current_path = new Path();
+$current_path->vertices[] = $vertices[0];
 /**
  * Paths is an array of all unique paths
  **/
 $paths = array();
+store_path($current_path, $paths);
 
-array_push($stack, $vertices[0);
+while (!empty($stack)) {
 
-while (!is_empty($stack)) {
+    echo "\nWe are looking for $looking_for_vertex" . PHP_EOL;
 
-    $current = array_pop($stack);
+    $edge_traveled = array_pop($stack);
+    //mark this edge as visited
+    $visited_edges[] = $edge_traveled->id;
 
-    //remove any visited edges
-    $edges = array_filter($current->get_edges(), function($edge) {
-        return !in_array($visited_edges, $edge);
-    });
 
-    if (count($edges) > 0) {
+    /**
+     * We need to ensure that vertex_a of this edge is the last vertex in our 
+     * current path. If not, change the current path to have that
+     **/
+     if (count($current_path->vertices) > 0 && 
 
-        //sort the edges in highest weight to shortest weight
-        $edges = uasort($edges, function($edge_a, $edge_b) {
-            if ($edge_a->weight == $edge_b->weight) {
-                return 0;
+         $current_path->vertices[count($current_path->vertices) - 1]->index 
+            != $edge_traveled->vertex_a->index) {
+
+         $tmp_vertices = $current_path->vertices;
+
+         while ($tmp_vertices[count($tmp_vertices) - 1]->index 
+             != $edge_traveled->vertex_a->index) {
+             array_pop($tmp_vertices);
+         }
+
+         $current_path = retrieve_path($tmp_vertices, $paths);
+     }
+
+
+    echo "length of proposed path is " . 
+        ($current_path->length + $edge_traveled->weight) . 
+        " and shortest is " . 
+        ($shortest_path == false ? -1 : $shortest_path->length) . PHP_EOL;
+
+    if (!$shortest_path || $shortest_path->length > ($current_path->length + $edge_traveled->weight)) {
+
+        $current_path->add($edge_traveled);
+        $current_vertex = $edge_traveled->vertex_b;
+
+        //store the length of this new path
+        store_path($current_path, $paths);
+
+
+        //have we found the vertex we are looking for??
+        if ($current_vertex->index == $looking_for_vertex) {
+            echo "found our index!" . PHP_EOL;
+            //YES, so check if this is the shortest
+            if (!$shortest_path || $current_path->length < $shortest_path->length) {
+                echo "its the shortest with {$current_path->length}!" . PHP_EOL;
+                $shortest_path = clone $current_path;
             }
 
-            return $edge_a->weight > $edge_b->weight ? -1 : 1;
-        });
+            //move back to the previous vertex
+            $current_vertex = $edge_traveled->vertex_a;
+            $tmp_vertices = $shortest_path->vertices;
+            //remove the last vertex
+            array_pop($tmp_vertices);
+            $current_path = retrieve_path($tmp_vertices, $paths);
+            echo "retrieved a path with length " . $current_path->length;
+            echo "\n\n\n";
+        } 
 
-        //push all unvisited edges on
-        foreach ($edges as $edge) {
-            array_push($stack, $vertices[$edge->vertext_b]);
-        }
+        //remove any visited edges
+        echo "Possible edges" . PHP_EOL;
+        $possible_edges = array_filter($current_vertex->edges,
+            function($edge) use ($visited_edges) {
+                return !in_array($edge->id, $visited_edges);
+            });
 
-        $shortest_edge = $edges[count($edges) - 1];
-        $current_path->add($vertices[$shortest_edge->vertext_b], $shortest_edge);
-        $visited_edges[] = $shortest_edge;
-
-        if ($shortest_edge->vertext_b == $looking_for_vertex) {
-            if ($shortest == -1 || $shortest > $current_path->length) {
-                $shortest = $current_path->length;
+        //do we have any non visited edges to take
+        echo "found " . count($possible_edges) . " possible edges." . PHP_EOL;
+        if (count($possible_edges) > 0) {
+            uasort($possible_edges, 'edge_sort');
+            foreach ($possible_edges as $edge) {
+                $stack[] = $edge;
             }
-        } else {
         }
-    } else {
-        echo "No unvisited edges from vertex {$current->index}" . PHP_EOL;
-    }
+    } 
+
 }
+
+echo "THE SHORTEST PATH IS " . $shortest_path->length;
+
